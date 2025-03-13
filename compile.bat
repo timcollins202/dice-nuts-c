920b1759199a181612e5ae3e34a1680f21f69825
Tim C. <timcollins202@gmail.com>
2025-03-12 23:26:53 -0400

    Adding some dice rolling functions

--- compile.bat
+++ compile.bat
@@ -8,16 +8,18 @@ set CC65_HOME=..\
 
 :: Compile all C files to assembly
 cc65 -Oirs %name%.c --add-source -o build/%name%.s
+cc65 -Oirs src/gameplay.c --add-source -o build/gameplay.s
 cc65 -Oirs src/graphics.c --add-source -o build/graphics.s
 cc65 -Oirs src/player_actions.c --add-source -o build/player_actions.s
 
 :: Assemble all .s files to object files
 ca65 crt0.s
 ca65 build/%name%.s -g
+ca65 build/gameplay.s -g
 ca65 build/graphics.s -g
 ca65 build/player_actions.s -g
 
-ld65 -C nrom_32k_vert.cfg -o %name%.nes crt0.o build/%name%.o build/graphics.o build/player_actions.o nes.lib -Ln labels.txt --dbgfile dbg.txt
+ld65 -C nrom_32k_vert.cfg -o %name%.nes crt0.o build/%name%.o build/gameplay.o build/graphics.o build/player_actions.o nes.lib -Ln labels.txt --dbgfile dbg.txt
 
 del *.o
 
---
+++ src/gameplay.c
@@ -0,0 +1,38 @@
+#include "../lib/neslib.h"
+#include "../lib/nesdoug.h"
+
+#define MAX_DICE 6
+
+unsigned char dice_values[MAX_DICE];    //stores the dice values we have rolled
+unsigned char dice_selected[MAX_DICE];  //1 if selected, 0 if not
+
+unsigned char random_number;            //random value 1-6 we'll use as a die roll
+unsigned char toggle_index;             //set before calling toggle_die_selection()
+
+void roll_die(void) 
+{
+    do{
+        random_number = rand8() & 0x07;     //mask off the lowest 3 bits 0-7
+    } while (random_number >= 5);           //reject values 6 and 7 (leaving 0-5)
+
+    random_number += 1;                     //shift range to 1-6
+}
+
+void roll_unselected_dice(void) 
+{
+    unsigned char i;
+    
+    for ( i = 0; i < MAX_DICE; i++) {
+        if (!dice_selected[i]) {
+            roll_die();
+            dice_values[i] = (random_number);
+        }
+    }
+}
+
+void toggle_die_selection(void) 
+{
+    if (toggle_index < MAX_DICE) {
+        dice_selected[toggle_index] ^= 1;   //toggle between 0 and 1
+    }
+}
